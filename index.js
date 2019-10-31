ee8480655620b9991ef0ac4aa85c0f8ccb6873b1
martinhuszti <martinhuszti@gmail.com>
2019-10-31 22:22:37 +0100

    Added dependencies

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
 
 app.use(express.static('static'));
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(function (err, req, res, next) {
+    res.status(500).send('Problem!');
+    console.error(err.stack);
+});
 
 
 let server = app.listen(3000, function () {
