ee5c1a55a63176c6297aeb37c815ee8b87a58944
Martin <martinhuszti@gmail.com>
2019-11-07 23:55:41 +0100

    Created lovalhost Mongo Db String

--- middleware/config/db.js
+++ middleware/config/db.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
-const connection_string = require('./urlstring');
+
+/**
+ * Mongo Atlas URL in urlstring.js file
+ * @type {string}
+ */
+//const connection_string = require('./urlstring');
+
+const connection_string = "localhost:27017/OCUW5I";
 
 mongoose.connect(connection_string, {
     useNewUrlParser: true,
